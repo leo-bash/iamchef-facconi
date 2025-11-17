08e5240d6176d9b734c128b02d855841d5040812
leo-bash <facconi.leonardo1@gmail.com>
2025-11-17 17:28:19 +0100

    feat: add SelectedIngredient and SuggestItem components with props, and implement SuggestList to render suggestions

---
+++ src/components/suggest/SuggestItem.tsx
@@ -0,0 +1,19 @@
+import type { IngredientInterface } from "../../types/ingredients";
+
+type SuggestItemProps = {
+  ingredient: IngredientInterface;
+  handleClick: (ing: IngredientInterface) => void;
+};
+
+const SuggestItem = ({ ingredient, handleClick }: SuggestItemProps) => {
+  return (
+    <div
+      onClick={() => handleClick(ingredient)}
+      className="bg-white rounded-lg p-2 cursor-pointer"
+    >
+      <h1>{ingredient.name}</h1>
+    </div>
+  );
+};
+
+export default SuggestItem;
