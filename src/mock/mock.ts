f56012b009d01840cf76c169ec80c0bcd9381bfe
leo-bash <facconi.leonardo1@gmail.com>
2025-11-17 17:32:10 +0100

    feat: add environment variables for API key and base URL, and create fallback recipe mock data

---
+++ src/mock/mock.ts
@@ -0,0 +1,12 @@
+import type { RecipeInterface } from "../types/recipes";
+
+export const fallbackRecipeMock: RecipeInterface = {
+  id: 1,
+  title: "Sushi Rolls",
+  image:
+    "https://spoonacular.com/recipeImages/660211-556x370.jpg",
+  readyInMinutes: 45,
+  servings: 4,
+  pricePerServing: 350,
+  healthScore: 65,
+};
